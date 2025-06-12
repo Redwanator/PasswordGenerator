2e9c3bbea57ecb1b9cbbf80d36a8c7a9b35df47f
Redwan <neo_dante_9@hotmail.com>
2025-06-12 12:20:12 +0200

    Ajout de commentaires XML à IUserInteractionService
    Ajout de documentation XML pour toutes les méthodes de l'interface IUserInteractionService. Chaque méthode est maintenant accompagnée d'une description détaillée, incluant les paramètres et les valeurs de retour, ce qui améliore la lisibilité et la compréhension du code.

--- PasswordGenerator/Interfaces/IUserInteractionService.cs
+++ PasswordGenerator/Interfaces/IUserInteractionService.cs
@@ -7,7 +7,25 @@ namespace PasswordGenerator.Interfaces;
 /// </summary>
 internal interface IUserInteractionService
 {
-    int AskInt(string message, int min, int max);           // DemanderEntier()
-    bool AskYesNo(string message);                          // DemanderOuiNon()
-    ReplayOption GetReplayOption();                         // ObtenirOptionRejouer()
+    /// <summary>
+    /// Demande à l'utilisateur un entier dans une plage spécifiée.
+    /// </summary>
+    /// <param name="message">Le message à afficher.</param>
+    /// <param name="min">La valeur minimale autorisée.</param>
+    /// <param name="max">La valeur maximale autorisée.</param>
+    /// <returns>Un entier compris entre min et max.</returns>
+    int AskInt(string message, int min, int max);   // DemanderEntier()
+
+    /// <summary>
+    /// Demande à l'utilisateur une réponse oui/non.
+    /// </summary>
+    /// <param name="message">Le message à afficher.</param>
+    /// <returns>True si l'utilisateur répond oui, sinon false.</returns>
+    bool AskYesNo(string message);                  // DemanderOuiNon()
+
+    /// <summary>
+    /// Demande à l'utilisateur s'il souhaite rejouer et retourne son choix.
+    /// </summary>
+    /// <returns>Une option représentant l'intention de l'utilisateur.</returns>
+    ReplayOption GetReplayOption();                 // ObtenirOptionRejouer()
 }
